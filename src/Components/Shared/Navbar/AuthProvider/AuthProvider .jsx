1bc8d32d75cd7cbcb1e4a11e2f647dc575133ce5
jubayer-ahmed-sajid <jubayerahmed661ac@gmail.com>
2024-12-20 20:28:51 +0600

    authprovider setup complete

--- src/Components/Shared/Navbar/AuthProvider/AuthProvider .jsx
+++ src/Components/Shared/Navbar/AuthProvider/AuthProvider .jsx
@@ -1,11 +1,64 @@
-import React from 'react';
+import React, { createContext, useEffect, useState } from "react";
+import app from "../../../FirebaseConfig";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth/web-extension";
 
-const AuthProvider  = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+export const AuthContext = createContext();
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const auth = getAuth(app);
+  const googleProvider = new GoogleAuthProvider();
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const googleSignin = () => {
+    return signInWithPopup(googleProvider);
+  };
+  const login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const logout = () => {
+    return signOut(auth);
+  };
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+      // if(currentUser){
+      //     axios.post('http://localhost:5000/jwt',{email:currentUser.email})
+      //     .then((data)=>{
+      //       if(data.data){
+      //         localStorage.setItem("access-token",data?.data?.token)
+      //         setLoading(false)
+      //       }
+      //     })
+      //   }
+      //   else{
+      //     localStorage.removeItem("access-token");
+      //     setLoading(false);
+      //   }
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
+  const authInfo = { createUser, googleSignin, login, logout, user, loading };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
-export default AuthProvider ;
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react'
+import { StrictMode } from "react";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import './index.css'
-import App from './App.jsx'
-import { RouterProvider } from 'react-router-dom';
-import routes from './Routes/Routes.jsx';
-
+import "./index.css";
+import App from "./App.jsx";
+import { RouterProvider } from "react-router-dom";
+import routes from "./Routes/Routes.jsx";
+import AuthProvider from "./Components/Shared/Navbar/AuthProvider/AuthProvider .jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={routes} />
+    <AuthProvider>
+      <RouterProvider router={routes} />
+    </AuthProvider>
   </React.StrictMode>
 );
