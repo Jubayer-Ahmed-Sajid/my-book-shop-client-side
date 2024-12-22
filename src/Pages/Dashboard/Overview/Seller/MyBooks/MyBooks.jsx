f4fa25ed348910f419a7d841800a0a4652136c49
jubayer-ahmed-sajid <jubayerahmed661ac@gmail.com>
2024-12-23 00:19:56 +0600

    implement BasicTable for displaying seller's books

--- src/Pages/Dashboard/Overview/Seller/MyBooks/MyBooks.jsx
+++ src/Pages/Dashboard/Overview/Seller/MyBooks/MyBooks.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import UseAuth from "../../../../../Hooks/UseAuth";
 import useSellerBooks from "../../../../../Hooks/useSellerBooks";
 import Loading from "../../../../../Components/Loading";
+import { FaEdit, FaTrashAlt } from 'react-icons/fa';
+import BasicTable from "../../../../../Components/BasicTable";
+import { Link } from "react-router-dom";
 
 const MyBooks = () => {
   const { user } = UseAuth();
@@ -14,8 +17,67 @@ const MyBooks = () => {
       </div>
     );
   }
-  console.log(data?.data);
-  return <div></div>;
+
+  const columns = [
+    {
+        header: 'Title',
+        accessorKey: 'title',
+    },
+    {
+        header: 'Author',
+        accessorKey: 'author',
+    },
+    {
+        header: 'Category',
+        accessorKey: 'category',
+    },
+    {
+        header: 'Email',
+        accessorKey: 'email',
+    },
+    {
+        header: 'Image',
+        accessorKey: 'image',
+        cell: ({ row }) => (
+            <img src={row.original.image} alt={row.original.title} width={70} />
+        ),
+    },
+    {
+        header: 'Price',
+        accessorKey: 'price',
+    },
+    {
+        header: 'Stock',
+        accessorKey: 'stock',
+    },
+    {
+        header: 'Update',
+        accessorKey: 'update',
+        cell: ({ row }) => (
+            <button onClick={() => handleUpdate(row.original)}>
+               <Link to={`/dashboard/update/${row.original._id}`}><FaEdit /></Link> 
+            </button>
+        ),
+    },
+    {
+        header: 'Delete',
+        accessorKey: 'delete',
+        cell: ({ row }) => (
+            <button onClick={() => handleDelete(row.original)}>
+                <FaTrashAlt />
+            </button>
+        ),
+    },
+];
+const handleUpdate = (book) => {
+    console.log(book);
+}
+const handleDelete = (data) => {
+   
+}
+  return <div>
+    <BasicTable data={data?.data} columns={columns} />
+  </div>;
 };
 
 export default MyBooks;
