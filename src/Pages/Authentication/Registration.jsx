76228e39d2f73ec663409de24b291db4df6bdca9
jubayer-ahmed-sajid <jubayerahmed661ac@gmail.com>
2024-12-26 12:25:11 +0600

    feat: enhance Registration component with improved UI elements and form validation; add icons for better user experience

--- src/Pages/Authentication/Registration.jsx
+++ src/Pages/Authentication/Registration.jsx
@@ -9,13 +9,16 @@ import UseAuth from "../../Hooks/UseAuth";
 import { toast } from "sonner";
 import { FcGoogle } from "react-icons/fc";
 import PageTitle from "../../Components/PageTitle";
+import { MdOutlineMail } from "react-icons/md";
+import { CgProfile } from "react-icons/cg";
+import { IoKeyOutline } from "react-icons/io5";
 
 // image hosting key and api
 const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
 const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const Registration = () => {
-  const { createUser, googleSignin} = UseAuth();
+  const { createUser, googleSignin } = UseAuth();
   const axiosPublic = useAxiosPublic();
   const navigate = useNavigate();
   const location = useLocation();
@@ -34,7 +37,9 @@ const Registration = () => {
       fullName: Yup.string()
         .max(25, "Must be 15 characters or less")
         .required("Name is Required"),
-      email: Yup.string().email("Invalid email address").required("Email is Required"),
+      email: Yup.string()
+        .email("Invalid email address")
+        .required("Email is Required"),
       password: Yup.string()
         .required("Password is required")
         .min(8, "Password is too short - should be 8 chars minimum.")
@@ -48,13 +53,14 @@ const Registration = () => {
     onSubmit: async (values) => {
       const minDelay = 1000;
       const startTime = Date.now();
-      toast.loading("User creating...")
+      toast.loading("User creating...");
 
-      const{name,email,role,password} = values;
+      const { email, role, password } = values;
+      const name = values?.fullName;
       const image = values?.Profile;
       const cart = [];
       const wishlist = [];
-      const status = values?.role =='seller' ? "pending" : "approved";
+      const status = values?.role == "seller" ? "pending" : "approved";
 
       // image hosting and url creation
       const formData = new FormData();
@@ -67,7 +73,7 @@ const Registration = () => {
       // User creation with email and password
       try {
         const createdUser = await createUser(email, password);
-       console.log(createdUser)
+        console.log(createdUser);
         const isAdmin = false;
         const userInfo = {
           name,
@@ -84,108 +90,127 @@ const Registration = () => {
         });
         const passed = Date.now() - startTime;
         if (passed < minDelay) {
-          await new Promise((resolve) => setTimeout(resolve, minDelay - passed));
+          await new Promise((resolve) =>
+            setTimeout(resolve, minDelay - passed)
+          );
         }
-       
+
         toast.dismiss();
         toast.success("User successfully created!!");
         navigate(location?.state ? location?.state : "/");
-        
       } catch (err) {
-        toast.dismiss()
-        toast.error(`${err.message}`)
+        toast.dismiss();
+        toast.error(`${err.message}`);
       }
     },
   });
-
 
   const handleGoogleSignin = async () => {
     const res = await googleSignin();
     try {
       const name = res?.user?.displayName;
       const email = res?.user.email;
-      const photoURL =res?.user?.photoURL;
-      const role = "buyer"
-      const status = "approved"
+      const photoURL = res?.user?.photoURL;
+      const role = "buyer";
+      const status = "approved";
       const isAdmin = false;
-      const cart=[];
-      const wishlist=[];
+      const cart = [];
+      const wishlist = [];
 
-      const userInfo= {name,email,photoURL,role,status,isAdmin,cart,wishlist} 
+      const userInfo = {
+        name,
+        email,
+        photoURL,
+        role,
+        status,
+        isAdmin,
+        cart,
+        wishlist,
+      };
       const minDelay = 1000;
       const startTime = Date.now();
       toast.loading("User is creating...");
 
-      await axiosPublic.post("/users", userInfo)
+      await axiosPublic.post("/users", userInfo);
 
       const passed = Date.now() - startTime;
       if (passed < minDelay) {
         await new Promise((resolve) => setTimeout(resolve, minDelay - passed));
       }
-     
+
       toast.dismiss();
       toast.success("User successfully created!!");
-      
-      
     } catch (error) {
-      toast.dismiss()
-      toast.error(`${error.message}`)
+      toast.dismiss();
+      toast.error(`${error.message}`);
     }
   };
 
   return (
-    <div className=" w-full my-8 mx-12">
+    <div className="mx-auto mt-4 mb-8 lg:mx-12">
       <PageTitle title={"Registration"}></PageTitle>
 
       <form
         onSubmit={formik.handleSubmit}
-        className="rounded-lg mt-12 flex flex-col gap-2 items-center bg-black py-6 space-y-2 mx-auto lg:w-2/3 text-white"
+        className="rounded-lg flex flex-col gap-2 items-center bg-secondary py-6 space-y-2 mx-auto lg:w-2/3 text-white"
       >
-        <h2 className="text-center text-2xl lg:text-4xl text-[#d9f9a5] my-6">
-          Please Sign Up{" "}
+        <h2 className="text-center text-2xl lg:text-4xl text-accent_1 my-6">
+          Please Register
         </h2>
         <div className=" w-3/4 lg:w-2/4 space-y-2">
-          <label className="" htmlFor="fullName">
-            Full Name
+          Name
+          <br />
+          <label
+            className="input input-bordered flex items-center gap-2"
+            htmlFor="fullName"
+          >
+            <CgProfile className="text-secondary" />
+
+            <input
+              id="fullName"
+              name="fullName"
+              type="text"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.fullName}
+              className="grow text-secondary"
+              placeholder=" Your full Name"
+            />
           </label>
-          <br />
-          <input
-            id="fullName"
-            name="fullName"
-            type="text"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.fullName}
-            className="w-full mx-auto rounded-[7px] border border-blue-gray-200  bg-transparent px-3 py-2.5 text-sm  text-blue-secondary outline outline-0 transition-all  focus:border-pink-base-300  focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-            placeholder=" Your full Name"
-          />
-          <br />
           {formik.touched.fullName && formik.errors.fullName ? (
             <p className="text-red-400 text-md">{formik.errors.fullName}</p>
           ) : null}
         </div>
         <div className="w-3/4 lg:w-2/4 mx-auto space-y-2">
-          <label htmlFor="email"> Email Address </label>
+          Email
           <br />
+          <label
+            htmlFor="email"
+            className="input input-bordered flex items-center gap-2"
+          >
+            <MdOutlineMail className="text-secondary"></MdOutlineMail>
 
-          <input
-            id="email"
-            name="email"
-            type="email"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.email}
-            className="w-full rounded-[7px] border border-blue-gray-200  bg-transparent px-3 py-2.5 text-sm  text-blue-secondary outline outline-0 transition-all  focus:border-pink-base-300  focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-            placeholder="Email Address"
-          />
+            <input
+              id="email"
+              name="email"
+              type="email"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
+              className="grow text-secondary"
+              placeholder="Email Address"
+            />
+          </label>
           {formik.touched.email && formik.errors.email ? (
             <p className="text-red-400 text-md">{formik.errors.email}</p>
           ) : null}
         </div>
 
         <div className="w-3/4 lg:w-2/4 mx-auto space-y-2">
-          <label>Image</label>
-          <br />
+          <label className="">
+            Profile Photo
+            <br />
+          </label>
 
           <input
             id="image"
@@ -198,20 +223,25 @@ const Registration = () => {
             onBlur={formik.handleBlur}
           />
         </div>
-        <br />
+
         <div className="space-y-2 mx-auto w-3/4 lg:w-2/4">
-          <label htmlFor="password">Password</label>
-          <br />
-          <input
-            id="password"
-            name="password"
-            type="password"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.password}
-            className="w-full rounded-[7px] border border-blue-gray-200  bg-transparent px-3 py-2.5 text-sm  text-blue-secondary outline outline-0 transition-all  focus:border-pink-base-300  focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-            placeholder="password"
-          />
+          Password <br />
+          <label
+            className="input input-bordered flex items-center gap-2"
+            htmlFor="password"
+          >
+            <IoKeyOutline className="text-secondary" />
+            <input
+              id="password"
+              name="password"
+              type="password"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.password}
+              className="grow text-secondary"
+              placeholder="password"
+            />
+          </label>
           {formik.touched.password && formik.errors.password ? (
             <p className="text-red-400 text-md">{formik.errors.password}</p>
           ) : null}
@@ -220,7 +250,7 @@ const Registration = () => {
           <label>Role</label>
           <br />
           <select
-            className="select text-black select-bordered w-full"
+            className="select text-secondary select-bordered w-full"
             id="role"
             name="role"
             onChange={formik.handleChange}
@@ -238,22 +268,22 @@ const Registration = () => {
         <br />
         <div className="w-3/4 flex items-center justify-center lg:w-2/4">
           <button
-            className="w-3/4 btn py-3 rounded-lg  px-3 bg-primary text-white"
+            className="w-full btn py-3 rounded-lg  px-3 bg-accent_1 border-none text-white"
             type="submit"
           >
             Sign up
           </button>
         </div>
 
-        <div className="divider divider-accent lg:w-2/3 mx-auto">Or</div>
+        <div className="divider divider-accent lg:w-2/3 w-full mx-auto">Or</div>
 
         <div className="w-3/4 flex items-center justify-center lg:w-2/4">
           <button
             type="button"
             onClick={handleGoogleSignin}
-            className="btn gap-2 flex items-center mb-4 text-white rounded-lg text-center w-3/4 bg-primary py-3"
+            className="btn gap-2 flex items-center mb-4 text-white rounded-lg text-center w-full bg-accent_1 border-none py-3"
           >
-            <FcGoogle className="text-2xl"/>
+            <FcGoogle className="text-2xl" />
             <h2 className="flex justify-center items-center gap-4">
               Login by Google
             </h2>
@@ -269,7 +299,6 @@ const Registration = () => {
           </h2>
         </div>
       </form>
-
     </div>
   );
 };
